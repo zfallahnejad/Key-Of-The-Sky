696892439c7aa633dec014de8d820d7800257661
n-zare <n_zare_m@yahoo.com>
2013-10-19 09:12:12 +0330

    edited
    error.php, index.php, login.php has been added

---
+++ keyOfSky-phpcode/protected/views/site/index.php
@@ -0,0 +1,15 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<h1 align="right">به سیستم کلید آسمان خوش آمدید</h1>
+
+
+<img  align="middle"  width="900" height="500" src="https://mail-attachment.googleusercontent.com/attachment/u/0/?ui=2&ik=e8d3a333cd&view=att&th=141c1ea6c7d5d168&attid=0.1&disp=inline&realattid=6e9b048c62e7a6d_0.1&safe=1&zw&saduie=AG9B_P-N9-V2hbxKCBpwvHCrZvcT&sadet=1381938013053&sads=1Md0uaCrh9CVOtgIq1Zkv7Qeekk&sadssc=1" alt="مسجد"/>
+
+
+
+<!--a href="http://localhost/skykeyy/skyKeey/index.php?r=site/login">ورود</a>
+<a href="http://www.yiiframework.com/doc/">ثبت نام</a> */
