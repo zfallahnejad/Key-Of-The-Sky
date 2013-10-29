9a82dabb045298eb0b889e7a1b72e9456523c5e4
k-sadjadi <sk_sajadi@ymail.com>
2013-10-29 23:51:09 +0330

    The code has been commited

---
+++ Phase1/Key Of Sky-phpcode/protected/views/site/login.php
@@ -0,0 +1,51 @@
+<?php 
+/* @var $this SiteController */
+/* @var $model LoginForm */
+/* @var $form CActiveForm  */
+
+$this->pageTitle=Yii::app()->name . ' - Login';
+$this->breadcrumbs=array(
+	'Login',
+);
+?>
+
+<h1 align="right">ورود</h1>
+
+<p align="right">لطفا فرم زیر را با اطلاعات مناسب پر کنید</p>
+
+<div class="form">
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'login-form',
+	'enableClientValidation'=>true,
+	'clientOptions'=>array(
+		'validateOnSubmit'=>true,
+	),
+)); ?>
+
+	<p align="right" class="note">فیلدهای دارای<span class="required">*</span> لازم هستند.</p>
+
+	<div align="right" class="row">
+		<?php echo $form->labelEx($model,'username'); ?>
+		<?php echo $form->textField($model,'username'); ?>
+		<?php echo $form->error($model,'username'); ?>
+	</div>
+
+	<div align="right" class="row">
+		<?php echo $form->labelEx($model,'password'); ?>
+		<?php echo $form->passwordField($model,'password'); ?>
+		<?php echo $form->error($model,'password'); ?>
+		
+	</div>
+
+	<div align="right" class="row rememberMe">
+		<?php echo $form->checkBox($model,'rememberMe'); ?>
+		<?php echo $form->label($model,'rememberMe'); ?>
+		<?php echo $form->error($model,'rememberMe'); ?>
+	</div>
+
+	<div align="right" class="row buttons">
+		<?php echo CHtml::submitButton('ورود'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+</div><!-- form -->
