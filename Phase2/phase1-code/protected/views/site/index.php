d1bd620ba808e62e270a0e041c1b239ddbe234cd
k-sadjadi <sk_sajadi@ymail.com>
2013-10-30 09:10:06 +0330

    The code has been commited

---
+++ Phase2/phase1-code/protected/views/site/index.php
@@ -0,0 +1,130 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<h1 align="right"><font size=5.5 color="#8d2796">به سیستم کلید آسمان خوش آمدید</font></h1>
+
+
+
+
+
+
+<div class="row-fluid">
+	<div class="span9">
+		<div class="row-fluid">
+			<div class="span11">
+				<div class="carousel slide" id="control10">
+					<div class="carousel-inner">
+						<div class="item active">
+							<img src="./images/key5-1.jpg" alt="No image found" style="width:100%;height:315px;" />
+							<div class="carousel-caption">
+								<h4 class="auto-style1">
+									
+								</h4>
+								<p style="font-size:x-small" class="auto-style1">
+									</p>
+							</div>
+						</div>
+						<div class="item">
+							<img src="./images/2.jpg" alt="No image found" style="width:100%;height:315px;" />
+							<div class="carousel-caption">
+								<h4 class="auto-style1">
+									
+								</h4>
+								<p style="font-size:x-small" class="auto-style1">
+									</p>
+							</div>
+						</div>
+						<div class="item">
+												<img src="./images/3.jpg" alt="No image found" style="width:100%;height:315px;" />
+												<div class="carousel-caption">
+													<h4 class="auto-style1">
+													
+													</h4>													
+													<p style="font-size:x-small" class="auto-style1">
+													</p>
+												</div>
+											</div>
+											<div class="item">
+												<img src="./images/key-of-sky2.jpg" alt="No image found" style="width:100%;height:315px;" />
+												<div class="carousel-caption">
+													<h4 class="auto-style1">
+													
+													</h4>													
+													<p style="font-size:x-small" class="auto-style1">
+													</p>
+												</div>
+											</div>
+
+					</div>
+					<a class="left carousel-control" href="#control10" data-slide="prev">
+						<img src="./images/30px-39.png" /></a><a class="right carousel-control" href="#control10" data-slide="next"><img src="./images/30px-40.png" /></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+	
+	
+	
+	<div class="span3">
+							<div id="control4" class="accordion">
+								<div class="accordion-group">
+									<div class="accordion-heading">
+										<a class="accordion-toggle" data-toggle="collapse" data-parent="#control4" href="#control4-0">
+										<div class="auto-style1" align="right">
+											<span lang="fa">درباره ما</span></div>
+										</a>
+									&nbsp;</div>
+									<div id="control4-0" class="accordion-body collapse in">
+										<div class="auto-style2" align="right" style="height: 90px">
+											
+<p align="right"><font color="#8634a0" size="2">  کلید آسمان سامانه ای اینترنتی برای ثبت، امتیازدهی و تشویق فعالیت های فرهنگی کودکان و نوجوانان در مساجد است. این سیستم برای اجرای طرح «سفیران مسجد» فراهم گردیده است  </font> </p>
+
+
+
+
+											
+												
+												
+												
+												
+											
+										</div>
+									</div>
+								</div>
+								<div class="accordion-group">
+									<div class="accordion-heading" align="right">
+										<a lang="fa" class="accordion-toggle" data-toggle="collapse" data-parent="#control4" href="#control4-4">لینک های مفید</a>
+									</div>
+									<div id="control4-4" class="accordion-body collapse">
+										<div class="accordion-inner" style="height: 70px">
+											
+											<br />
+											
+										</div>
+									</div>
+								</div>
+								<div class="accordion-group">
+									<div class="accordion-heading" align="right">
+										<a class="accordion-toggle" data-toggle="collapse" data-parent="#control4" href="#control4-5">لینک مدارس شرکت کننده</a>
+									</div>
+									<div id="control4-5" class="accordion-body collapse">
+										<div class="accordion-inner" style="height: 70px">
+											<br /></div>
+									</div>
+								</div>
+							
+						
+						
+</div>
+
+
+<!--<img  align="middle"  width="900" height="500" src="./images/key5-1.jpg" alt="کلید آسمان"/> 
+-->
+
+
+
