a6ec57f60654aa2d5bbfa7680e2b8c4950add527
f-movahedi <fmovahedi255@gmail.com>
2013-11-09 22:56:49 +0330

    phase1-code deleted

--- Phase2/phase1-code/Scripts/bootstrap/_vti_cnf/bootstrap-alert.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_author:SR|ZOHREH\\Dell
-vti_modifiedby:SR|ZOHREH\\Dell
-vti_timelastmodified:TR|16 Oct 2013 07:47:12 -0000
-vti_timecreated:TR|16 Oct 2013 07:47:12 -0000
-vti_cacheddtm:TX|16 Oct 2013 07:47:12 -0000
-vti_filesize:IR|2444
-vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|SignUp.html key.html Login.html
