e74fed8f4bfc2533789f0b9c0554f5fb1ba50505
f-movahedi <f.movahedi71@yahoo.com>
2013-11-29 20:09:18 +0330

    forms updated

--- Phase3/Phase3 Code/protected/models/GivePointForm.php
+++ Phase3/Phase3 Code/protected/models/GivePointForm.php
@@ -1,3 +1,43 @@
 <?php
 
-?>
+class GivePointForm extends CFormModel
+{
+	public $actTopic;
+	public $actPoint;
+	public $actId;
+	public $stCode;
+	
+	/**
+	 * Declares the validation rules.
+	 */
+	public function rules()
+	{
+		return array(
+			// verifyCode are required
+			//array('verifyCode', 'required'),
+			// verifyCode needs to be entered correctly
+			//array('verifyCode', 'captcha', 'allowEmpty'=>!CCaptcha::checkRequirements()),
+			array('actTopic','required'),
+			
+			
+		);
+	}
+	
+
+
+	
+	/**
+	 * Declares customized attribute labels.
+	 * If not declared here, an attribute would have a label that is
+	 * the same as its name with the first letter in upper case.
+	 */
+	 public function attributeLabels()
+	 {
+		return array(
+			'actTopic'=>'',
+			
+			
+		);
+	}
+	
+}
